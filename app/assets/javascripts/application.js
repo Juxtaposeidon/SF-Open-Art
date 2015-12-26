cc26de8e2b288e83d9878a9f2c60048fd524ac67
dbcjckyee <dbcjckyee@gmail.com>
2015-12-25 18:05:51 -0800

    set up view with initial map that shows current location

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+    });
+    return map;
+  }
+  // google.maps.event.addDomListener(window, "load", initMap);
+  var map = new initMap()
+  navigator.geolocation.getCurrentPosition(function (position){
+  initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  dropMarker(position.coords.latitude, position.coords.longitude, "!")
+  map.setCenter(initialLocation)
+  });
+
+
+  function dropMarker(lati, longi, symbol){
+    var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: lati, lng: longi},
+    label: symbol
+    });
+  }
+});
--- app/controllers/nearsearch_controller.rb
+++ app/controllers/nearsearch_controller.rb
@@ -1,2 +1,7 @@
 class NearsearchController < ApplicationController
+
+  def index
+    p params
+  end
+
 end
