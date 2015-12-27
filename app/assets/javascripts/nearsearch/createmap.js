5447750684b1afe2c400e84aa5944db5311f5501
dbcjckyee <dbcjckyee@gmail.com>
2015-12-26 16:50:44 -0800

    ajax call now returns a partial with a google map that displays on the nearsearch page

---
+++ app/assets/javascripts/nearsearch/createmap.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+    });
+    return map;
+  }
+
+  var map = new initMap()
+  navigator.geolocation.getCurrentPosition(function (position){
+  initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  dropMarker(position.coords.latitude, position.coords.longitude, "!")
+  map.setCenter(initialLocation)
+  });
+
+
+  function dropMarker(lati, longi, symbol){
+    var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: lati, lng: longi},
+    label: symbol
+    });
+  }
+
+});
--- app/assets/javascripts/nearsearch/nearsearch.js
+++ app/assets/javascripts/nearsearch/nearsearch.js
@@ -1,27 +1,7 @@
 $(document).ready(function() {
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 13,
-    });
-    return map;
-  }
-
-  var map = new initMap()
   navigator.geolocation.getCurrentPosition(function (position){
-  initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  dropMarker(position.coords.latitude, position.coords.longitude, "!")
-  map.setCenter(initialLocation)
   findNearEvent(position)
   });
-
-
-  function dropMarker(lati, longi, symbol){
-    var marker = new google.maps.Marker({
-    map: map,
-    position: {lat: lati, lng: longi},
-    label: symbol
-    });
-  }
 
   function findNearEvent(location){
     var position = {
@@ -32,6 +12,9 @@ $(document).ready(function() {
         data: position,
         method: "GET",
         url: 'nearsearch/locate'
+    })
+    .done(function(result){
+      $('#content').html(result['partial'])
     })
   }
 
