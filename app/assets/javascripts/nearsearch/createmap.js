35df3a14d5a2950df12a6777c864d4b421706a9c
dbcjckyee <dbcjckyee@gmail.com>
2015-12-26 18:57:05 -0800

    remove map marker drop function, add code to draw directions between current location and nearest art piece

--- app/assets/javascripts/nearsearch/createmap.js
+++ app/assets/javascripts/nearsearch/createmap.js
@@ -1,25 +1,40 @@
 $(document).ready(function() {
+  var map = new initMap()
+  var startlat = 0
+  var startlong = 0
+
   function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 13,
     });
     return map;
   }
-  console.log($('#map').data('longitude'))
-  var map = new initMap()
   navigator.geolocation.getCurrentPosition(function (position){
-  initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  dropMarker(position.coords.latitude, position.coords.longitude, "!")
-  map.setCenter(initialLocation)
+  // initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  // map.setCenter(initialLocation)
+  startlat = position.coords.latitude
+  startlong = position.coords.longitude
+  displayRoute();
   });
 
+  function displayRoute() {
+    console.log(startlat)
+    var start = new google.maps.LatLng(startlat, startlong);
+    var end = new google.maps.LatLng($('#map').data('latitude'), $('#map').data('longitude'));
 
-  function dropMarker(lati, longi, symbol){
-    var marker = new google.maps.Marker({
-    map: map,
-    position: {lat: lati, lng: longi},
-    label: symbol
+    var directionsDisplay = new google.maps.DirectionsRenderer();// also, constructor can get "DirectionsRendererOptions" object
+    directionsDisplay.setMap(map); // map should be already initialized.
+    var request = {
+        origin : start,
+        destination : end,
+        travelMode : google.maps.TravelMode.DRIVING
+    };
+    var directionsService = new google.maps.DirectionsService();
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        }
     });
-  }
+}
 
 });
