36f7a325170a585e8e0e65f7676891c8cae333aa
dbcjckyee <dbcjckyee@gmail.com>
2015-12-30 14:23:44 -0800

    build out top bar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.precompile += %w( nearsearch/createmap.js )
 Rails.application.config.assets.precompile += %w( shownearby/createmap.js )
 Rails.application.config.assets.precompile += %w( shownearby/shownearbyload.js )
 Rails.application.config.assets.precompile += %w( artistsearch/createmap.js )
+Rails.application.config.assets.precompile += %w( artlogo.png )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
