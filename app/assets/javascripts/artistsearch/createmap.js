23c72623426c7eeeb9338a213e9dd2353b35e1f0
dbcjckyee <dbcjckyee@gmail.com>
2015-12-29 17:56:17 -0800

    work on show page for artistsearch so that the map displays a marker for the piece of art the user clicks on. the map also centers around the marker

--- app/assets/javascripts/artistsearch/createmap.js
+++ app/assets/javascripts/artistsearch/createmap.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
   var map
+  initMap();
+  map.setCenter(new google.maps.LatLng(37.7833, -122.4167));
   var startlat = 0
   var startlong = 0
-
+  var marker = new google.maps.Marker({
+    map: map,
+  });
   function initMap() {
     map = new google.maps.Map(document.getElementById('artistmap'), {
       zoom: 13
     });
   }
-
-  initMap();
-  map.setCenter(new google.maps.LatLng(37.7833, -122.4167));
+  function changeMarkerPosition(tag, lat, lng) {
+      var latlng = new google.maps.LatLng(lat, lng);
+      tag.setPosition(latlng);
+  }
+  $('.artname').click(function(event){
+    event.preventDefault();
+    changeMarkerPosition(marker, $(this).data('latitude'), $(this).data('longitude'))
+    map.setCenter(new google.maps.LatLng($(this).data('latitude'), $(this).data('longitude')));
+  })
 })
