8ea1ee0df688ecc7b08f98c81d454e6f6b19b5e3
dbcjckyee <dbcjckyee@gmail.com>
2015-12-30 21:16:25 -0800

    fix views for artistsearch and farsearch so that the results are on the right and the map is on the left

--- app/controllers/farsearch_controller.rb
+++ app/controllers/farsearch_controller.rb
@@ -4,5 +4,6 @@ class FarsearchController < ApplicationController
 
   def search
     @result = Place.near(params["query"], 10, :order => "distance").limit(10)
+    @getresults = @result.length > 0
   end
 end
