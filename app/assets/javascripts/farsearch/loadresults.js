082020e84c353d2df7e3f31a2728db724d52f51d
dbcjckyee <dbcjckyee@gmail.com>
2015-12-30 20:43:56 -0800

    do basic styling for farsearch. add map logic to farsearch (working state)

--- app/assets/javascripts/farsearch/loadresults.js
+++ app/assets/javascripts/farsearch/loadresults.js
@@ -8,22 +8,22 @@ $(document).ready(function() {
     map: map,
   });
   var infowindow = new google.maps.InfoWindow();
+
   function initMap() {
-    map = new google.maps.Map(document.getElementById('artistmap'), {
-      zoom: 13
+    map = new google.maps.Map(document.getElementById('resultmap'), {
+      zoom: 16
     });
   }
   function changeMarkerPosition(tag, lat, lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    tag.setPosition(latlng);
+  var latlng = new google.maps.LatLng(lat, lng);
+  tag.setPosition(latlng);
   }
-  $('.artname').click(function(event){
+  $('.result').click(function(event){
     event.preventDefault();
-    map.setZoom(16)
     changeMarkerPosition(marker, $(this).data('latitude'), $(this).data('longitude'))
     map.setCenter(new google.maps.LatLng($(this).data('latitude'), $(this).data('longitude')));
     infowindow.setContent("<i>"+ $(this).text() + "</i>: " + $(this).data('address'))
     infowindow.open(map,marker);
     scroll(0,0)
   })
-})
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.precompile += %w( shownearby/createmap.js )
 Rails.application.config.assets.precompile += %w( shownearby/shownearbyload.js )
 Rails.application.config.assets.precompile += %w( artistsearch/createmap.js )
 Rails.application.config.assets.precompile += %w( artlogo.png )
+Rails.application.config.assets.precompile += %w( farsearch/loadresults.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
