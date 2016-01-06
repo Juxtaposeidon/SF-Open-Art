e41eb0675c2299b9065342afd729b365b6c01a55
dbcjckyee <dbcjckyee@gmail.com>
2016-01-06 14:12:03 -0800

    precompile assets

---
+++ public/assets/nearbysearch/pageinteractions-53b0cd9541c0c030dde134c82e51943cf347a38fdc046a56897c8bde6f48e8a3.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+  var map = new google.maps.Map(document.getElementById('nearmap'), {
+    zoom: 16
+  });
+  var startlat;
+  var startlong;
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var marker = new google.maps.Marker({
+    map: map,
+    label: "A"
+  });
+
+  function changeMarkerPosition(tag, lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    tag.setPosition(latlng);
+  }
+  function displayRoute(end) {
+    var start = new google.maps.LatLng(startlat, startlong);
+    directionsDisplay.setMap(map);
+    var request = {
+      origin : start,
+      destination : end,
+      travelMode : google.maps.TravelMode.DRIVING
+    };
+    var directionsService = new google.maps.DirectionsService();
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+      }
+    });
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    startlat = position.coords.latitude
+    startlong = position.coords.longitude
+    initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    changeMarkerPosition(marker, position.coords.latitude, position.coords.longitude)
+    map.setCenter(initialLocation)
+  });
+
+  $("#locationframe").on("click", ".address", function(event){
+    event.preventDefault();
+    $('p').removeClass("selected")
+    $(this).parent().addClass("selected")
+    marker.setMap(null)
+    directionsDisplay.set('directions', null)
+    displayRoute(new google.maps.LatLng($(this).data('lat'), $(this).data('long')));
+    scroll(0,0)
+  })
+
+  $("#locationframe").on("click", "#next", function(event){
+    event.preventDefault();
+    $.ajax({
+      url: 'nearbysearch/nextresults',
+      method: "GET",
+    })
+    .done(function(results){
+      $('#locationframe').html(results["partial"])
+    })
+  });
+
+  $("#locationframe").on("click", "#prev", function(event){
+    event.preventDefault();
+    $.ajax({
+      url: 'nearbysearch/prevresults',
+      method: "GET",
+    })
+    .done(function(results){
+      $('#locationframe').html(results["partial"])
+    })
+  });
+
+});
