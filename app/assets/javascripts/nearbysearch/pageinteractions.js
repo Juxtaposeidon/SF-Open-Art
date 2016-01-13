c37fe80018b8ab33a48e627f2e7066fde02832a2
dbcjckyee <dbcjckyee@gmail.com>
2016-01-12 18:40:48 -0800

    refactor nearbysearch page results algorithm to eliminate class variable from the controller.

--- app/assets/javascripts/nearbysearch/pageinteractions.js
+++ app/assets/javascripts/nearbysearch/pageinteractions.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
       pagedir: $(this).text()
     }
     $.ajax({
-      url: 'nearbysearch/show',
+      url: '$(this).attr('href')',
       method: "GET",
       data: direction
     })
--- app/controllers/nearbysearch_controller.rb
+++ app/controllers/nearbysearch_controller.rb
@@ -2,11 +2,10 @@ class NearbysearchController < ApplicationController
   def index
     if request.xhr?
       @resultindex = 1
-      @@resultspage = 0
       @showprev = false
       @shownext = true
       @@allspots = Place.near([params['lat'], params['long']], 10, :order => "distance").limit(100)
-      @nearbyspots = Place.near([params['lat'], params['long']], 10, :order => "distance").limit(100)[@@resultspage..@@resultspage+9]
+      @nearbyspots = Place.near([params['lat'], params['long']], 10, :order => "distance").limit(100)[@resultindex-1..@resultindex+8]
       render :json => {
         :partial => render_to_string(:partial => 'nearbysearch/map')
       }
@@ -15,15 +14,10 @@ class NearbysearchController < ApplicationController
 
   def show
     p params
-    if params['pagedir'] == 'next'
-      @@resultspage += 10
-    else
-      @@resultspage -= 10
-    end
-    @resultindex = @@resultspage + 1
-    @showprev = @@resultspage > 0
-    @shownext = @@resultspage < 90
-    @nearbyspots = @@allspots[@@resultspage..@@resultspage+9]
+    @resultindex = params[:id].to_i
+    @showprev = @resultindex > 1
+    @shownext = @resultindex < 90
+    @nearbyspots = @@allspots[@resultindex - 1..@resultindex + 8]
     render :json => {
       :partial => render_to_string(:partial => 'nearbysearch/locations')
     }
