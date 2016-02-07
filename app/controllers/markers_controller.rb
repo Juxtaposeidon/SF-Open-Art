33a87831281bff9ad6316a03f470981133f826e2
dbcjckyee <dbcjckyee@gmail.com>
2016-02-06 21:53:49 -0800

    fix fullmap pins not showing up

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -2,14 +2,13 @@ class MarkersController < ApplicationController
 
   def index
     if request.xhr?
-      render(json:  Place.all)
+      render(json: Location.all)
     end
   end
 
   def show
     if request.xhr?
-      p params
-      render(json: Place.find(params['markerid']))
+      render(json: Location.find(params['markerid']))
     end
   end
 
