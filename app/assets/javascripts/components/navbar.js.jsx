7ad3a76c59ff858f9706470b46fcce85f435eff2
dbcjckyee <dbcjckyee@gmail.com>
2016-02-06 21:21:52 -0800

    finish live draft of navbar in react

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -2,16 +2,18 @@ var Navbar = React.createClass({
   getInitialState: function(){
     return{
       links: this.props.tabs,
+      logolink: this.props.logolink,
+      logo: this.props.image
     }
   },
   render: function(){
     var tabs = this.state.links
+    console.log(this.state.logo)
     linktext = tabs.map(function(tab){
       key = tabs.indexOf(tab)
       return <NavTab link={tab.link}
       text={tab.text}
-      tabkey={key}
-      />
+      tabkey={key}/>
     })
     return(
         <header>
@@ -20,6 +22,7 @@ var Navbar = React.createClass({
             <tbody>
               <tr id="menurow">
                   <td id ="logotd">
+                  <a href={this.state.logolink}><img src={this.state.logo} id="logo"></img></a>
                     </td>
                 {linktext}
               </tr>
