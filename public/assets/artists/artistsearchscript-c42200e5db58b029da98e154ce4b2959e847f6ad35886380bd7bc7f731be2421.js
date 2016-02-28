68a82ced82c60e3144205336e82951c1d290b820
dbcjckyee <dbcjckyee@gmail.com>
2016-02-27 21:28:24 -0800

    copmile assets

---
+++ public/assets/artists/artistsearchscript-c42200e5db58b029da98e154ce4b2959e847f6ad35886380bd7bc7f731be2421.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var map = new google.maps.Map(document.getElementById('artistmap'), {
+    zoom: 13
+  });
+  map.setCenter(new google.maps.LatLng(37.7833, -122.4167));
+  var marker = new google.maps.Marker({
+    map: map,
+  });
+  var infowindow = new google.maps.InfoWindow();
+  function changeMarkerPosition(tag, lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    tag.setPosition(latlng);
+  }
+
+  $('#artistresults').on("click",".artname", function(event){
+    map.setZoom(16)
+    changeMarkerPosition(marker, $(this).data('latitude'), $(this).data('longitude'))
+    map.setCenter(new google.maps.LatLng($(this).data('latitude'), $(this).data('longitude')));
+    infowindow.setContent("<i>"+ $(this).text() + "</i>: " + $(this).data('address'))
+    infowindow.open(map,marker);
+    scroll(0,0)
+  })
+})
