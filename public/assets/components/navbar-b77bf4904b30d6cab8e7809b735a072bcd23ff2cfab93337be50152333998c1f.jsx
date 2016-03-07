c570d74431a4f783e90fd857f99b246c37640dcb
dbcjckyee <dbcjckyee@gmail.com>
2016-03-06 23:25:56 -0800

    comp assets

---
+++ public/assets/components/navbar-b77bf4904b30d6cab8e7809b735a072bcd23ff2cfab93337be50152333998c1f.jsx
@@ -0,0 +1,54 @@
+var Navbar = React.createClass({
+  getInitialState: function(){
+    return{
+      links: this.props.tabs,
+      logolink: this.props.logolink,
+      logo: this.props.image
+    }
+  },
+
+  render: function(){
+    var tabs = this.state.links
+    var linktext = tabs.map(function(tab){
+      tabindex = tabs.indexOf(tab)
+      return(
+        <NavTab
+          link={tab.link}
+          key={tabindex}
+          text={tab.text}
+        />
+      )
+    })
+    return(
+      <header>
+        <table id="menutable">
+          <tbody>
+            <tr id="menurow">
+                <td id ="logotd">
+                <a href={this.state.logolink}><img src={this.state.logo} id="logo"></img></a>
+                  </td>
+              {linktext}
+            </tr>
+          </tbody>
+        </table>
+      </header>
+    )
+  }
+})
+
+var NavTab = React.createClass({
+  getInitialState: function(){
+    return {
+      link: this.props.link,
+      text: this.props.text,
+    }
+  },
+
+  render: function(){
+    return(
+      <td className="menuitem">
+        <a href={this.state.link}>{this.state.text}</a>
+      </td>
+    )
+  }
+})
---
+++ public/assets/locations/searchresultmapinteractivity-53416410ad3e7ee67e3eae449abdc0645e5477a9301dc87bbd31f695defc4058.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var map = new google.maps.Map(document.getElementById('resultmap'), {
+    zoom: 13
+  });
+  map.setCenter(new google.maps.LatLng(37.7833, -122.4167));
+  var marker = new google.maps.Marker({
+    map: map
+  });
+  var infowindow = new google.maps.InfoWindow();
+  function changeMarkerPosition(tag, lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    tag.setPosition(latlng);
+  }
+  $('#results').on("click", ".result", function(event){
+    changeMarkerPosition(marker, $(this).data('latitude'), $(this).data('longitude'))
+    map.setCenter(new google.maps.LatLng($(this).data('latitude'), $(this).data('longitude')));
+    infowindow.setContent("<i>"+ $(this).text() + "</i>: " + $(this).data('address'))
+    infowindow.open(map,marker);
+    scroll(0,0)
+    map.setZoom(16)
+  })
+});
